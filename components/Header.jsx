9592ed9021983dbf3da7d70760d85130af8347fa
mosespace <kisakyemoses50@gmail.com>
2024-01-21 23:55:20 +0300

    feat: added a new header component

---
+++ components/Header.jsx
@@ -0,0 +1,41 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import ToggleBtn from "@/app/components/ToggleBtn";
+
+export default function Header() {
+  return (
+    <div>
+      {/*Desktop Primary Nav Bar */}
+      <nav className='flex flex-col justify-between py-[1rem] lg:text-3xl text-2xl font-bold text-gray-900 dark:text-white px-4 lg:px-0'>
+        <div className='flex lg:flex-row gap-3 lg:gap-0 flex-col lg:justify-between lg:items-center'>
+          <Link
+            href='/'
+            className='flex lg:items-center lg:flex-row flex-col gap-3'
+          >
+            <Image
+              width={300}
+              height={300}
+              src='/me.jpg'
+              className='lg:w-14 w-full object-cover h-14 lg:rounded-full'
+              alt='Kisakye Moses Full-Stack Software Developer Located in Kampala Uganda.'
+            />
+            <span className='flex flex-col text-xl'>
+              <h2 className='uppercase '>Kisakye Moses</h2>
+              <span className='text-sm'>Senior Front-End Developer</span>
+            </span>
+          </Link>
+          <div className='flex gap-[1rem] items-center lg:justify-center text-xl'>
+            <div className='flex items-center gap-1 rounded-full bg-slate-50 dark:bg-transparent dark:border-[1.5px] py-2 px-4'>
+              <div className='flex justify-center items-center w-5 h-5 bg-green-200 dark:bg-green-500 animate-pulse rounded-full'>
+                <div className='relative inline-flex w-3 h-3 bg-green-500 dark:bg-green-200 rounded-full'></div>
+              </div>
+              <span className='text-sm'>Online</span>
+            </div>
+            <ToggleBtn />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
