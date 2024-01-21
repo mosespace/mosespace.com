33b012f3b93533202258cc508be280459af5f858
mosespace <kisakyemoses50@gmail.com>
2024-01-21 23:56:15 +0300

    feat: initial delete💥

--- app/page.js
+++
@@ -1,9 +0,0 @@
-import Hero from "./components/Hero";
-import PageWrapper from "./components/page-wrapper";
-export default function Home() {
-  return (
-    <PageWrapper>
-      <Hero />
-    </PageWrapper>
-  );
-}
