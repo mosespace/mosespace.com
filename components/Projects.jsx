c50bc26c6000196f32582318c347c87c1d2c792e
mosespace <kisakyemoses50@gmail.com>
2024-01-21 23:56:36 +0300

    feat: initial commit💥

---
+++ components/Projects.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div className='py-12'>
+      <div className='m-auto lg:m-0 px-4 lg:px-0 text-gray-600 md:px-12'>
+        <div className='mb-12 space-y-2k text-start'>
+          <h2 className='text-3xl font-bold text-gray-800 md:text-4xl dark:text-white'>
+            Sharing is Caring
+          </h2>
+          <p className='text-gray-600 dark:text-gray-300'>
+            Quam hic dolore cumque voluptate rerum beatae et quae, tempore sunt,
+            debitis dolorum officia aliquid explicabo? Excepturi, voluptate?
+          </p>
+        </div>
+
+        <div className='mx-auto'>
+          <div className='group relative -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300 hover:z-10'>
+            <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
+              <img
+                src='https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
+                alt='art cover'
+                loading='lazy'
+                width='1000'
+                height='667'
+                className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
+              />
+            </div>
+
+            <div className='sm:p-2 sm:pl-0 sm:w-4/6'>
+              <span className='mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0'>
+                Jul 27 2022
+              </span>
+              <h3 className='text-2xl font-semibold text-gray-800 dark:text-white'>
+                Provident de illo eveniet commodi fuga fugiat laboriosam
+                expedita.
+              </h3>
+              <p className='my-6 text-gray-600 dark:text-gray-300'>
+                Laudantium in, voluptates ex placeat quo harum aliquam totam,
+                doloribus eum impedit atque...
+              </p>
+
+              <div className='flex gap-4'>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  Tailwindcss
+                </a>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  VueJS
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
+            <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
+              <img
+                src='https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
+                alt='art cover'
+                loading='lazy'
+                width='1000'
+                height='667'
+                className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
+              />
+            </div>
+
+            <div className='sm:p-2 sm:pl-0 sm:w-4/6'>
+              <span className='mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0'>
+                Jul 27 2022
+              </span>
+              <h3 className='text-2xl font-semibold text-gray-800 dark:text-white'>
+                Provident de illo eveniet commodi fuga fugiat laboriosam
+                expedita.
+              </h3>
+              <p className='my-6 text-gray-600 dark:text-gray-300'>
+                Laudantium in, voluptates ex placeat quo harum aliquam totam,
+                doloribus eum impedit atque...
+              </p>
+
+              <div className='flex gap-4'>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  Tailwindcss
+                </a>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  VueJS
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
+            <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
+              <img
+                src='https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
+                alt='art cover'
+                loading='lazy'
+                width='1000'
+                height='667'
+                className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
+              />
+            </div>
+
+            <div className='sm:p-2 sm:pl-0 sm:w-4/6'>
+              <span className='mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0'>
+                Jul 27 2022
+              </span>
+              <h3 className='text-2xl font-semibold text-gray-800 dark:text-white'>
+                Provident de illo eveniet commodi fuga fugiat laboriosam
+                expedita.
+              </h3>
+              <p className='my-6 text-gray-600 dark:text-gray-300'>
+                Laudantium in, voluptates ex placeat quo harum aliquam totam,
+                doloribus eum impedit atque...
+              </p>
+
+              <div className='flex gap-4'>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  Tailwindcss
+                </a>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                >
+                  VueJS
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
