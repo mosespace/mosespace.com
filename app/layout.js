9eb6b4132be25e049295996df1b65e7beca51c8d
mosespace <kisakyemoses50@gmail.com>
2024-01-22 10:14:28 +0300

    feat: initial commit💥

--- app/layout.js
+++ app/layout.js
@@ -8,6 +8,7 @@ import { ToastContainer } from "react-toastify";
 import Provider from "./context/Provider";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import ScrollTop from "@/components/ScrollTop";
 
 export const metadata = {
   title: "Kisakye Moses | Mosespace",
@@ -24,6 +25,7 @@ export default function RootLayout({ children }) {
           <div className='dark:bg-[#030712] bg-white w-full overflow-hidden'>
             <div className='container max-w-4xl m-auto space-y-8 px-6 py-12 text-zinc-950 dark:text-white md:px-12 lg:px-20'>
               <Header />
+              <ScrollTop />
               {children}
               <Footer />
             </div>
