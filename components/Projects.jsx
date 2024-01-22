a599c13174aa2e6b73f174ec791107a2196162e1
mosespace <kisakyemoses50@gmail.com>
2024-01-22 10:14:42 +0300

    feat: initial commit💥

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,18 +1,19 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Projects() {
   return (
-    <div className='py-12'>
+    <div className='py-8'>
       <div className='m-auto lg:m-0 px-4 lg:px-0 text-gray-600 md:px-12'>
         <div className='mx-auto'>
-          <div className='group relative -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300 hover:z-10'>
+          <div className='group relative -mx-4 mb-3 sm:-mx-8 lg:lg:p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300 hover:z-10'>
             <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
-              <img
-                src='https://images.unsplash.com/photo-1661749711934-492cd19a25c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
-                alt='art cover'
+              <Image
+                src='/project_images/proj8.jpg'
+                alt='Kisakye Moses Project 1'
                 loading='lazy'
                 width='1000'
-                height='667'
+                height='100'
                 className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
               />
             </div>
@@ -33,28 +34,28 @@ export default function Projects() {
               <div className='flex gap-4'>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  Tailwindcss
+                  Code
                 </a>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  VueJS
+                  Live Preview
                 </a>
               </div>
             </div>
           </div>
 
-          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
+          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 lg:p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
             <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
-              <img
-                src='https://images.unsplash.com/photo-1491895200222-0fc4a4c35e18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80'
-                alt='art cover'
+              <Image
+                src='/project_images/proj8.jpg'
+                alt='Kisakye Moses Project 1'
                 loading='lazy'
                 width='1000'
-                height='667'
+                height='100'
                 className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
               />
             </div>
@@ -75,28 +76,28 @@ export default function Projects() {
               <div className='flex gap-4'>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  Tailwindcss
+                  code
                 </a>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  VueJS
+                  Live Preview
                 </a>
               </div>
             </div>
           </div>
 
-          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
+          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 lg:p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
             <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
-              <img
-                src='https://images.unsplash.com/photo-1620121692029-d088224ddc74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80'
-                alt='art cover'
+              <Image
+                src='/project_images/proj8.jpg'
+                alt='Kisakye Moses Project 1'
                 loading='lazy'
                 width='1000'
-                height='667'
+                height='100'
                 className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
               />
             </div>
@@ -117,15 +118,56 @@ export default function Projects() {
               <div className='flex gap-4'>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  Tailwindcss
+                  code
                 </a>
                 <a
                   href='#'
-                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-white dark:border-gray-700 dark:text-gray-300'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
                 >
-                  VueJS
+                  Live Preview
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className='group relative hover:z-10 -mx-4 mb-3 sm:-mx-8 lg:p-6 sm:p-8 rounded-3xl bg-white dark:bg-transparent border border-transparent hover:border-gray-100 dark:shadow-none dark:hover:border-gray-700 dark:hover:bg-gray-800 shadow-2xl shadow-transparent hover:shadow-gray-600/10 sm:gap-8 sm:flex transition duration-300'>
+            <div className='sm:w-2/6 rounded-3xl overflow-hidden transition-all duration-500 group-hover:rounded-xl'>
+              <Image
+                src='/project_images/proj8.jpg'
+                alt='Kisakye Moses Project 1'
+                loading='lazy'
+                width='1000'
+                height='100'
+                className='h-56 sm:h-full w-full object-cover object-top transition duration-500 group-hover:scale-105'
+              />
+            </div>
+
+            <div className='sm:p-2 sm:pl-0 sm:w-4/6'>
+              <span className='mt-4 mb-2 inline-block font-medium text-gray-400 dark:text-gray-500 sm:mt-0'>
+                Jul 27 2022
+              </span>
+              <h3 className='text-2xl font-semibold text-gray-800 dark:text-white'>
+                Provident de illo eveniet commodi fuga fugiat laboriosam
+                expedita.
+              </h3>
+              <p className='my-6 text-gray-600 dark:text-gray-300'>
+                Laudantium in, voluptates ex placeat quo harum aliquam totam,
+                doloribus eum impedit atque...
+              </p>
+
+              <div className='flex gap-4'>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
+                >
+                  code
+                </a>
+                <a
+                  href='#'
+                  className='px-3 py-1 rounded-full border border-gray-100 text-sm font-medium text-primary transition duration-300 hover:border-transparent hover:bg-primary hover:text-orange-600 dark:border-gray-700 dark:text-gray-300'
+                >
+                  Live Preview
                 </a>
               </div>
             </div>
